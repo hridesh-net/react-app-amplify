3d9a45dc44e51ca16f670c715632f7506a5c723b
Hridesh sharma <61771411+hridesh-net@users.noreply.github.com>
2022-11-12 02:43:43 +0530

    mew

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "reactappamplifyb25a9d76": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
